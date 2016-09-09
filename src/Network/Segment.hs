ae9c849494bc5691200f967c17e46b8494c5e450
Edward George <edwardgeorge@gmail.com>
2016-09-09 17:23:19 +0100

    feat: return the success value

--- src/Network/Segment.hs
+++ src/Network/Segment.hs
@@ -2,6 +2,7 @@
 module Network.Segment where
 import Control.Lens                   -- from: lens
 import Data.Aeson (toJSON)            -- from: aeson
+import Data.Aeson.Lens                -- from: lens-aeson
 import Data.Text (Text, unpack)       -- from: text
 import Data.Text.Strict.Lens          -- from: lens
 import Data.Thyme.Time                -- from: thyme
@@ -16,10 +17,12 @@ type WriteKey = Text
 apiEndpoint :: Format r (Text -> r)
 apiEndpoint = "https://api.segment.io/v1/" % stext
 
-sendEvent :: WriteKey -> Event -> IO ()
+sendEvent :: WriteKey -> Event -> IO Bool
 sendEvent wkey ev@(Event evt _ _) = do
   let url  = unpack . sformat apiEndpoint $ evt ^. re eventType
       opts = Wreq.defaults & Wreq.auth ?~ Wreq.basicAuth (wkey ^. re utf8) ""
   resp <- Wreq.postWith opts url (toJSON $ payload ev)
-  print resp
-  return ()
+  -- the docs say they _always_ return 200
+  case resp ^? Wreq.responseBody . key "success" . _Bool of
+    Just v -> return v
+    Nothing -> return False
