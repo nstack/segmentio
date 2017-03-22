e91704b4882e79118b05445b6df5db4d1d34ec4b
Edward George <edwardgeorge@gmail.com>
2017-03-22 09:52:47 +0000

    chore: remove dependency on formatting as it ends up pulling in libstdc++

--- src/Network/Segment.hs
+++ src/Network/Segment.hs
@@ -6,9 +6,9 @@ module Network.Segment (WriteKey,
 import Control.Lens                   -- from: lens
 import Data.Aeson (toJSON)            -- from: aeson
 import Data.Aeson.Lens                -- from: lens-aeson
+import Data.Monoid
 import Data.Text (Text, unpack)       -- from: text
 import Data.Text.Strict.Lens          -- from: lens
-import Formatting                     -- from: formatting
 import qualified Network.Wreq as Wreq -- from: wreq
 
 import Network.Segment.Reified
@@ -16,12 +16,12 @@ import Network.Segment.Types
 
 type WriteKey = Text
 
-apiEndpoint :: Format r (Text -> r)
-apiEndpoint = "https://api.segment.io/v1/" % stext
+apiEndpoint :: Text -> Text
+apiEndpoint stext = "https://api.segment.io/v1/" <> stext
 
 sendEvent :: WriteKey -> Event -> IO Bool
 sendEvent wkey ev = do
-  let url  = unpack . sformat apiEndpoint $ ev ^. eventType . eventTypeText
+  let url  = unpack . apiEndpoint $ ev ^. eventType . eventTypeText
       opts = Wreq.defaults & Wreq.auth ?~ Wreq.basicAuth (wkey ^. re utf8) ""
   resp <- Wreq.postWith opts url (toJSON $ payload ev)
   -- the docs say they _always_ return 200
